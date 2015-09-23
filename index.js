16de84011133a915c317edf9f962b00a481d1285
Francescu Santoni <francescu@stupeflix.com>
2015-09-23 14:18:44 +0200

    Added opencv

--- index.js
+++ index.js
@@ -10,4 +10,34 @@ var server = app.listen(8080, function () {
   var port = server.address().port;
 
   console.log('RPI app listening at http://%s:%s', host, port);
+  var cv = require('opencv');
+  // camera properties
+var camWidth = 320;
+var camHeight = 240;
+var camFps = 10;
+var camInterval = 1000 / camFps;
+
+// face detection properties
+var rectColor = [0, 255, 0];
+var rectThickness = 2;
+
+// initialize camera
+var camera = new cv.VideoCapture(0);
+camera.setWidth(camWidth);
+camera.setHeight(camHeight);
+
+    camera.read(function(err, im) {
+      if (err) throw err;
+
+      im.detectObject('./node_modules/opencv/data/haarcascade_frontalface_alt2.xml', {}, function(err, faces) {
+        if (err) throw err;
+        if (faces.length == 0) console.log("No face");
+
+        for (var i = 0; i < faces.length; i++) {
+          face = faces[i];
+          console.log("Face "+face);
+        }
+
+      });
+    });
 });
